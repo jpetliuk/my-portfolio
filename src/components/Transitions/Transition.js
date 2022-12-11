7d1a7ab2efc980ebb00f00e9989d1b5303c3c1f7
jpetliuk <jeremias.petliuk@gmail.com>
2022-12-11 15:53:38 -0300

    comming soon

---
+++ src/components/Transitions/Transition.js
@@ -0,0 +1,12 @@
+import "./Transition.css";
+
+const Transition = () => {
+  return (
+    <div id="transition">
+      <div className="transition1"></div>
+      <div className="transition2"></div>
+    </div>
+  );
+};
+
+export default Transition;
